babe35aafd5ad4ae3989cc6f6e0b3b8a61f6d09a
Matt Yao <myao@8thlight.com>
2022-03-29 22:28:32 -0500

    Change origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'capstone-svr.herokuapp.com/graphql'
+    origins ['http://localhost:3000']
 
     resource '*',
       headers: :any,
